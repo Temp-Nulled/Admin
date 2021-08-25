3e1bad32c3608ba0cb87f7e1a619cb716381cbd1
Temp-Nulled <81937494+Temp-Nulled@users.noreply.github.com>
2021-08-25 13:28:55 -0500

    Add files via upload

---
+++ Passwords.lua
@@ -0,0 +1,135 @@
+local textl = script.Parent.Parent.TextLabel
+script.Parent.MouseButton1Click:Connect(function()
+	if script.Parent.Parent.TheBox.Text ==  "_&%7^%6$?8-4?$2-#?---+&^8=7%-!$$5&8%+26@&#-968!^4^" then --Add your login key here.
+		if script.Parent.Parent.TheBox2.Text == "@8=7!$*4-2@346$%@68-%&4_#?9&9-99@$9_@&@8" then -- Add your second  login key here.
+			textl.Text = "Verifying Keys"
+			textl.TextTransparency = 0.9
+			wait()
+			textl.TextTransparency = 0.8
+			wait()
+			textl.TextTransparency = 0.7
+			wait()
+			textl.TextTransparency = 0.6
+			wait()
+			textl.TextTransparency = 0.5
+			wait()
+			textl.TextTransparency = 0.4
+			wait()
+			textl.TextTransparency = 0.3
+			wait()
+			textl.TextTransparency = 0.2
+			wait()
+			textl.TextTransparency = 0.1
+			wait()
+			textl.TextTransparency = 0
+			wait(3)
+			textl.TextTransparency = 0.1
+			wait()
+			textl.TextTransparency = 0.2
+			wait()
+			textl.TextTransparency = 0.3
+			wait()
+			textl.TextTransparency = 0.4
+			wait()
+			textl.TextTransparency = 0.5
+			wait()
+			textl.TextTransparency = 0.6
+			wait()
+			textl.TextTransparency = 0.7
+			wait()
+			textl.TextTransparency = 0.8
+			wait()
+			textl.TextTransparency = 0.9
+			wait()
+			textl.TextTransparency = 1
+			wait(5)
+			
+				
+			textl.Text = "Token 1 has been verified"		
+		textl.TextTransparency = 0.9
+		wait()
+		textl.TextTransparency = 0.8
+		wait()
+		textl.TextTransparency = 0.7
+		wait()
+		textl.TextTransparency = 0.6
+		wait()
+		textl.TextTransparency = 0.5
+		wait()
+		textl.TextTransparency = 0.4
+		wait()
+		textl.TextTransparency = 0.3
+		wait()
+		textl.TextTransparency = 0.2
+		wait()
+		textl.TextTransparency = 0.1
+		wait()
+		textl.TextTransparency = 0
+		wait(3)
+		textl.TextTransparency = 0.1
+		wait()
+		textl.TextTransparency = 0.2
+		wait()
+		textl.TextTransparency = 0.3
+		wait()
+		textl.TextTransparency = 0.4
+		wait()
+		textl.TextTransparency = 0.5
+		wait()
+		textl.TextTransparency = 0.6
+		wait()
+		textl.TextTransparency = 0.7
+		wait()
+		textl.TextTransparency = 0.8
+		wait()
+		textl.TextTransparency = 0.9
+		wait()
+		textl.TextTransparency = 1
+		wait(5)
+			
+				textl.Text = "Token 2 has been verified"
+				textl.TextTransparency = 0.9
+				wait()
+				textl.TextTransparency = 0.8
+				wait()
+				textl.TextTransparency = 0.7
+				wait()
+				textl.TextTransparency = 0.6
+				wait()
+				textl.TextTransparency = 0.5
+				wait()
+				textl.TextTransparency = 0.4
+				wait()
+				textl.TextTransparency = 0.3
+				wait()
+				textl.TextTransparency = 0.2
+				wait()
+				textl.TextTransparency = 0.1
+				wait()
+				textl.TextTransparency = 0
+				wait(3)
+				textl.TextTransparency = 0.1
+				wait()
+				textl.TextTransparency = 0.2
+				wait()
+				textl.TextTransparency = 0.3
+				wait()
+				textl.TextTransparency = 0.4
+				wait()
+				textl.TextTransparency = 0.5
+				wait()
+				textl.TextTransparency = 0.6
+				wait()
+				textl.TextTransparency = 0.7
+				wait()
+				textl.TextTransparency = 0.8
+				wait()
+				textl.TextTransparency = 0.9
+				wait()
+				textl.TextTransparency = 1
+				wait(5)
+			script.Parent.Parent.Parent.In.Visible = false
+			script.Parent.Parent.Parent.Frame.Visible = true
+		end
+	end	
+end)
